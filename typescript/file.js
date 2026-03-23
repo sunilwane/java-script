9a73cd5338f6befee9cb0b6781233c391d6398a6
sunilwane <sunilwane211@gmail.com>
2026-03-23 11:31:39 +0530

    Typescriot intialization complete

---
+++ typescript/file.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const x = 1;
+const y = "Hello, TypeScript!";
+console.log(x);
+console.log(y);
+//# sourceMappingURL=file.js.map
---
+++ typescript/file.ts
@@ -0,0 +1,5 @@
+const x: number = 1;
+const y: string = "Hello, TypeScript!";
+
+console.log(x);
+console.log(y);
